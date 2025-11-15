9837d05b10d2a40527ab2426a3c87d26f6f5fe7e
Leticia <160972155+Lety85@users.noreply.github.com>
2025-11-15 15:41:06 +0100

    Implement lambda functions for squaring numbers, filtering evens, and sorting products by price in Exercise 3

--- exercises/exercise_03/exercise_03.py
+++ exercises/exercise_03/exercise_03.py
@@ -9,3 +9,24 @@
 # - Convert map/filter to list using list()
 
 # Your code here:
+
+original_numbers = list(range(1,11))
+print(f'Original numbers: {original_numbers}')
+
+square = lambda x: x ** 2
+square_map = list(map(square, original_numbers))
+print(f'Squared numbers: {square_map}')
+
+even = lambda x: x % 2 == 0
+even_filter = list(filter(even, original_numbers))
+print(f'Even numbers: {even_filter}')
+
+products = [{
+    "name": "Monitor", "price": 150}, {
+        "name": "Mouse", "price": 30}, {
+            "name": "Keyboard", "price": 25
+        }]
+sorted_products = sorted(products, key = lambda x: x["price"])
+print(f'Products sorted by price:')
+for product in sorted_products:
+    print(f'{product["name"]} - ${product["price"]}')
