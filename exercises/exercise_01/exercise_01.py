1199798cbc03e1eee960222a433fccd63127a289
Leticia <160972155+Lety85@users.noreply.github.com>
2025-11-15 15:40:55 +0100

    Implement Person class with greeting method for exercise 1

--- exercises/exercise_01/exercise_01.py
+++ exercises/exercise_01/exercise_01.py
@@ -8,3 +8,22 @@
 # - The introduce method should return a formatted string
 
 # Your code here:
+
+class Person():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def greeting(self):
+        print(f'Hello, my name is {self.name} and I am {self.age} year old.')
+
+
+alice = Person("Alice", 30)
+bob = Person("Bob",25)
+
+alice.greeting()
+bob.greeting()
+
+        
+    
