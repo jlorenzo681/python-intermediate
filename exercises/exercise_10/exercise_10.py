241c799ffc357d93254b02d277b2418c04d7ea23
Leticia <160972155+Lety85@users.noreply.github.com>
2025-11-25 17:33:13 +0100

    Implement Fibonacci and even number generators with memory usage comparison

--- exercises/exercise_10/exercise_10.py
+++ exercises/exercise_10/exercise_10.py
@@ -10,3 +10,58 @@
 # - Use sys.getsizeof() to check memory usage
 
 # Your code here:
+
+import sys
+
+def fibonacci_generator(n):
+    a,b = 0,1
+    for _ in range(n):
+        yield a
+        a,b = b, a+b
+
+print("First 10 Fibonacci numbers: ")
+fibo = fibonacci_generator(10)
+print(", ".join(str(x) for x in fibo))
+
+def even_numbers(start, end):
+    for x in range(start,end + 1):
+        if x % 2 == 0:
+            yield x
+
+print("Even numbers from 1 to 20: ")
+even = even_numbers(1,20)
+print(", ".join(str(x) for x in even))
+
+
+print("Manual interaction: ")
+
+fibo2 = fibonacci_generator(10)
+
+print(f'Next: {next(fibo2)}')
+print(f'Next: {next(fibo2)}')
+print(f'Next: {next(fibo2)}')
+
+
+print(f'Generator expression(squares of evens): ')
+squares_gen = (x**2 for x in range(1,11) if x % 2 == 0)
+print(", ".join(str(x) for x in squares_gen))
+
+
+def numb_list(n):
+    return [x for x in range(n)]
+
+def numb_generator(n):
+    for x in range(n):
+        yield x
+
+large_list = numb_list(10000)
+large_gen = numb_generator(10000)
+
+list_size = sys.getsizeof(large_list)
+gen_size = sys.getsizeof(large_gen)
+
+print(f'\n Memory usage for 10000 numbers: ')
+print(f'List: {list_size}')
+print(f'Generator: {gen_size}')
+print(f'Lists uses {list_size/gen_size} x more memory than generator!')
+
