218ccb0f3d6d423beb19e4c977960e6aa0fb37b6
Leticia <160972155+Lety85@users.noreply.github.com>
2025-11-25 17:33:16 +0100

    Implement multiple inheritance with Animal, Flyable, and Swimmable classes

--- exercises/exercise_11/exercise_11.py
+++ exercises/exercise_11/exercise_11.py
@@ -9,3 +9,49 @@
 # - Order of inheritance matters!
 
 # Your code here:
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        return "some sound"
+
+
+class Flyable:
+    def fly(self):
+        return f'{self.name} is flying!'
+
+
+class Swimmable:
+    def swim(self):
+        return f'{self.name} is swimming!'
+
+
+class Duck(Animal, Flyable, Swimmable):
+    def __init__(self,name):
+        super().__init__(name)
+    def speak(self):
+        return f'{self.name} says Quack!'
+    
+
+class Penguin(Animal, Swimmable):
+    def __init__(self, name):
+        super().__init__(name)
+    def speak(self):
+        return f'{self.name} says Squawk!'
+    def fly(self):
+        return f'{self.name} cannot fly!'
+    
+
+donald = Duck("Donald")
+print(donald.speak())
+print(donald.fly())
+print(donald.swim())
+
+tux = Penguin("Tux")
+print(tux.speak())
+print(tux.swim())
+print(tux.fly())
+
+print("Duck MRO:", "->".join(cls.__name__ for cls in Duck.__mro__))
+print("Penguin MRO:", "->".join(cls.__name__ for cls in Penguin.__mro__))
